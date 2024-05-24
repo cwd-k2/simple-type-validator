8b2dc4e33788056455d1c0165a49264e53812ca7
cwd-k2 <kaito.augmented7th@gmail.com>
2024-05-24 14:50:26 +0900

    配列のバリデータも関数で書けるように

--- types.ts
+++ types.ts
@@ -1,20 +1,21 @@
+type PeelSingleV<T> = T extends [infer R] ? R : T;
+
 type UnorderedVs<T, K = T> = [K] extends [never]
   ? []
   : K extends K
-  ? [ArrOrBasicV<K>, ...UnorderedVs<Exclude<T, K>>]
+  ? [
+      ((arg: unknown) => arg is T) | ObjectLikeV<K>,
+      ...UnorderedVs<Exclude<T, K>>
+    ]
   : never;
 
-type ArrOrBasicV<T> = T extends (infer E)[]
-  ? { type: "array"; elem: ValidatorOf<E> }
-  : ((arg: unknown) => arg is T) | RecurseObjV<T>;
-
-type RecurseObjV<T> = T extends object
+type ObjectLikeV<T> = T extends object
   ? T extends null
     ? never
+    : T extends (infer E)[]
+    ? { type: "array"; elem: ValidatorOf<E> }
     : { [K in keyof T]-?: ValidatorOf<T[K]> }
   : never;
-
-type PeelSingleV<T> = T extends [infer R] ? R : T;
 
 type ValidatorOf<T> = PeelSingleV<UnorderedVs<T>>;
 
