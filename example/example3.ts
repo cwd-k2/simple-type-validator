dfbae2c9aae4597d0b8c4ecfb4c9fcea188714e3
cwd-k2 <kaito.augmented7th@gmail.com>
2024-05-28 22:31:47 +0900

    examples update

--- example/example3.ts
+++ example/example3.ts
@@ -1,6 +1,13 @@
 import type { ValidatorOf } from "..";
 import { is, validate } from "..";
 
+function isFormattedDateString(arg: unknown): arg is string {
+  if (typeof arg !== "string") return false;
+
+  const regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+  return regex.test(arg);
+}
+
 type PostPayload = {
   id: number;
   contents: string;
@@ -13,7 +20,7 @@ type UserPayload = {
   name: string;
   created_at: string;
   updated_at: string;
-  posts?: PostPayload[];
+  posts: PostPayload[];
 };
 
 const isPostPayload: ValidatorOf<PostPayload> = {
@@ -26,18 +33,22 @@ const isPostPayload: ValidatorOf<PostPayload> = {
 const isUserPayload: ValidatorOf<UserPayload> = {
   id: is.number,
   name: is.string,
-  created_at: is.string,
-  updated_at: is.string,
-  posts: [{ type: "array", elem: isPostPayload }, is.undefined],
+  created_at: isFormattedDateString,
+  updated_at: isFormattedDateString,
+  posts: { type: "array", elem: isPostPayload },
 };
 
 const badUserPayload = {
-  id: "not a number",
+  id: 1,
   name: "a user",
+  created_at: "2000-01-01 00:00:00",
+  updated_at: "fugafuga",
+  posts: [{}],
 };
 
 try {
-  validate<UserPayload>(isUserPayload)(badUserPayload);
+  const never = validate<UserPayload>(isUserPayload)(badUserPayload);
+  console.log(never);
 } catch (e) {
   console.error(e);
 }
